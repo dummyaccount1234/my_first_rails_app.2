182d5468aebf24d1c7dae52d26ee9bfa6e191a46
Prelang Builder <builder@prelang.com>
2014-04-04 01:22:54 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/windows_controller.rb
+++ app/controllers/windows_controller.rb
@@ -3,7 +3,7 @@ class WindowsController < ApplicationController
   #->Prelang (scaffolding:rails/scope_to_user)
   before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
 
-  before_action :set_window, only: [:show, :edit, :update, :destroy]
+  before_action :set_window, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /windows
   # GET /windows.json
