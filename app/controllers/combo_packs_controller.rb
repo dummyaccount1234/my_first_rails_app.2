2ac20f34720e334aedb3ffbe1ba1f95669585b06
Prelang Builder <builder@prelang.com>
2014-04-04 01:22:49 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/combo_packs_controller.rb
+++ app/controllers/combo_packs_controller.rb
@@ -3,7 +3,7 @@ class ComboPacksController < ApplicationController
   #->Prelang (scaffolding:rails/scope_to_user)
   before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
 
-  before_action :set_combo_pack, only: [:show, :edit, :update, :destroy]
+  before_action :set_combo_pack, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /combo_packs
   # GET /combo_packs.json
